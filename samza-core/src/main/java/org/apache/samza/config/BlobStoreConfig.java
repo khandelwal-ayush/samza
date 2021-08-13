272f1c864a9a9cbcdf7a5e279f45416f747f42b8
shekhars-li <shesharma@linkedin.com>
2021-08-13 14:41:22 -0700

    Add default blob store manager and admin factory
    RB=2755394
    
    RB=2755396
    G=samza-reviewers
    R=dchen1
    A=dchen1

--- samza-core/src/main/java/org/apache/samza/config/BlobStoreConfig.java
+++ samza-core/src/main/java/org/apache/samza/config/BlobStoreConfig.java
@@ -25,17 +25,19 @@ package org.apache.samza.config;
 public class BlobStoreConfig extends MapConfig {
 
   public static final String BLOB_STORE_MANAGER_FACTORY = "blob.store.manager.factory";
+  public static final String DEFAULT_BLOB_STORE_MANAGER_FACTORY = "com.linkedin.samza.ambry.AmbryBlobStoreManagerFactory";
   public static final String BLOB_STORE_ADMIN_FACTORY = "blob.store.admin.factory";
+  public static final String DEFAULT_BLOB_STORE_ADMIN_FACTORY = "com.linkedin.samza.ambry.AmbryBlobStoreAdminFactory";
   public BlobStoreConfig(Config config) {
     super(config);
   }
 
 
   public String getBlobStoreManagerFactory() {
-    return get(BLOB_STORE_MANAGER_FACTORY);
+    return get(BLOB_STORE_MANAGER_FACTORY, DEFAULT_BLOB_STORE_MANAGER_FACTORY);
   }
 
   public String getBlobStoreAdminFactory() {
-    return get(BLOB_STORE_ADMIN_FACTORY);
+    return get(BLOB_STORE_ADMIN_FACTORY, DEFAULT_BLOB_STORE_ADMIN_FACTORY);
   }
 }
